375ff6e15ee8dc0b569e2a0d931cc46788bad42d
nooblong <18128966990@163.com>
2025-01-20 16:53:06 +0800

    update

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,16 @@
+'use client'
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export async function get(url: string) {
+  const data = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'access-token': localStorage.getItem("token") === null ? 'null' : localStorage.getItem("token"),
+    }
+  })
 }
--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-    output: 'export'
+  output: 'export',
+  async rewrites() {
+    return [{
+      source: '/api/:path*',
+      destination: 'http://127.0.0.1:25565/:path*',
+    },
+    ]
+  }
 };
 
 export default nextConfig;
