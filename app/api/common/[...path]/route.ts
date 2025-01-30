81e5bb5335beb0fd898b4b45d587a612ed6252d1
nooblong <18128966990@163.com>
2025-01-30 16:44:01 +0800

    Create route.ts

---
+++ app/api/common/[...path]/route.ts
@@ -0,0 +1,33 @@
+import {NextRequest, NextResponse} from 'next/server';
+import {api} from "@/lib/utils";
+
+export async function GET(req: NextRequest, context: { params: { path: string[] } }) {
+  const backendPath = context.params.path.join('/');
+  const headers = Object.fromEntries(req.headers.entries());
+  const backendResponse = await fetch(`${api}/${backendPath}`, {
+    method: 'GET',
+    headers: {
+      ...headers,
+    },
+  });
+
+  const data = await backendResponse.text();
+  return new NextResponse(data, {status: backendResponse.status});
+}
+
+export async function POST(req: NextRequest, context: { params: { path: string[] } }) {
+  const searchParams = req.nextUrl.searchParams;
+  const {path} = context.params;
+  const backendPath = path.join('/');
+  const body = await req.text();
+  const headers = Object.fromEntries(req.headers.entries());
+  const backendResponse = await fetch(`${api}/${backendPath}?${searchParams}`, {
+    method: 'POST',
+    headers: {
+      ...headers,
+    },
+    body,
+  });
+  const data = await backendResponse.text();
+  return new NextResponse(data, {status: backendResponse.status});
+}
