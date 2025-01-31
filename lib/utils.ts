4c65e823e933c95baaa58f376c2fcafdefe0643d
nooblong <18128966990@163.com>
2025-02-01 05:38:12 +0800

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,4 +5,17 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export const api = "http://nooblong1.mycloudnas.com:8080"
+export const formatDate = (date: Date): string => {
+  const padZero = (num: number): string => num.toString().padStart(2, "0");
+
+  const year = date.getFullYear();
+  const month = padZero(date.getMonth() + 1); // 月份从 0 开始
+  const day = padZero(date.getDate());
+  const hours = padZero(date.getHours());
+  const minutes = padZero(date.getMinutes());
+  const seconds = padZero(date.getSeconds());
+
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+};
+
+export const api = "http://127.0.0.1:25566"
