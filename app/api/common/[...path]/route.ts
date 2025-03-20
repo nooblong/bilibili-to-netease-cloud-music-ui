e823ee2def741ba63b911d3cbd2db1af9e05d6e5
nooblong <18128966990@163.com>
2025-03-20 16:42:58 +0800

    fix: disable cache

--- app/api/common/[...path]/route.ts
+++ app/api/common/[...path]/route.ts
@@ -15,6 +15,7 @@ export async function GET(req: NextRequest, context: { params: Promise<{ path: s
 
   const data = await backendResponse.text();
   const respHeader = backendResponse.headers;
+  respHeader.set("Cache-Control", "no-store");
   return new NextResponse(data, {status: backendResponse.status, headers: respHeader});
 }
 
@@ -33,5 +34,6 @@ export async function POST(req: NextRequest, context: { params: Promise<{ path: 
   });
   const data = await backendResponse.text();
   const respHeader = backendResponse.headers;
+  respHeader.set("Cache-Control", "no-store");
   return new NextResponse(data, {status: backendResponse.status, headers: respHeader});
 }
