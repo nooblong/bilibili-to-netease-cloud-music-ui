28d553e4540a34141813fb2aafb1b27659bb8aba
nooblong <18128966990@163.com>
2025-08-06 09:13:26 +0800

    v.0.1

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ import {ThemedSiderV2} from "./components/layout/sider";
 import {ThemedTitleV2} from "./components/layout/title";
 import {Emoji} from "./pages/emoji";
 
-export const Api = "http://127.0.0.1:25565"
+export const Api = "http://107.75.218.120:25565"
 
 function App() {
   return (
